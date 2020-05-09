dd94059794d62d9b23bf36588bfdb0c881c7a5ac
zhongxunking <zhongxunking@163.com>
2020-05-09 16:14:12 +0800

    调整MutexLock-unlock.lua

--- src/main/resources/META-INF/server/redis/mutex-lock/MutexLock-unlock.lua
+++ src/main/resources/META-INF/server/redis/mutex-lock/MutexLock-unlock.lua
@@ -1,6 +1,6 @@
 -- KEYS: lockKey
--- ARGV: lockerId（不能包含'|'） messageChannel
--- return: true（正常解锁）；false（非正常解锁）
+-- ARGV: lockerId syncChannel
+-- return: true（成功）；false（失败，锁不存在或已经易主）
 
 -- 数据结构
 -- lockKey:
@@ -8,17 +8,18 @@
 
 local lockKey = KEYS[1];
 local lockerId = ARGV[1];
-local messageChannel = ARGV[2];
+local syncChannel = ARGV[2];
 -- 如果不持有锁，则无需解锁
 local owner = redis.call('hget', lockKey, 'owner');
 if (owner ~= lockerId) then
     if (owner == false) then
-        redis.call('publish', messageChannel, 0);
+        -- 发布同步消息
+        redis.call('publish', syncChannel, 0);
     end
     return false;
 end
 -- 解锁
 redis.call('del', lockKey);
--- 解锁成功，发布解锁消息
-redis.call('publish', messageChannel, 0);
+-- 发布同步消息
+redis.call('publish', syncChannel, 0);
 return true;
