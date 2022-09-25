9a6d5d481300aba5b262e65bc205f6ac54ea10a2
zhongxun <zhongxunking@163.com>
2022-09-25 21:26:36 +0800

    新值key转换器

--- src/main/java/org/antframework/sync/semaphore/SemaphoreContext.java
+++ src/main/java/org/antframework/sync/semaphore/SemaphoreContext.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * 作者：钟勋 (email:zhongxunking@163.com)
  */
 
@@ -14,16 +14,24 @@ import org.antframework.sync.extension.Server;
 import org.antframework.sync.semaphore.core.DefaultServerSemaphore;
 import org.antframework.sync.semaphore.support.SemaphoreServer;
 
+import java.util.function.Function;
+
 /**
  * 信号量上下文
  */
 public class SemaphoreContext {
+    // key转换器
+    private final Function<Object, String> keyConverter;
     // Sync执行器
     private final SyncExecutor syncExecutor;
     // 信号量服务端
     private final SemaphoreServer semaphoreServer;
 
-    public SemaphoreContext(SyncExecutor syncExecutor, Server server, long maxWaitTime) {
+    public SemaphoreContext(Function<Object, String> keyConverter,
+                            SyncExecutor syncExecutor,
+                            Server server,
+                            long maxWaitTime) {
+        this.keyConverter = keyConverter;
         this.syncExecutor = syncExecutor;
         this.semaphoreServer = new SemaphoreServer(server, maxWaitTime);
     }
@@ -35,10 +43,19 @@ public class SemaphoreContext {
      * @param totalPermits 许可总数
      * @return 信号量
      */
-    public Semaphore getSemaphore(String key, int totalPermits) {
-        if (key == null || totalPermits < 0) {
-            throw new IllegalArgumentException("key不能为null且totalPermits不能小于0");
+    public Semaphore getSemaphore(Object key, int totalPermits) {
+        if (totalPermits < 0) {
+            throw new IllegalArgumentException("totalPermits不能小于0");
         }
-        return new DefaultServerSemaphore(key, SyncUtils.newId(), totalPermits, syncExecutor, semaphoreServer);
+        return new DefaultServerSemaphore(convertKey(key), SyncUtils.newId(), totalPermits, syncExecutor, semaphoreServer);
+    }
+
+    // 转换key
+    private String convertKey(Object key) {
+        String convertedKey = keyConverter.apply(key);
+        if (convertedKey == null) {
+            throw new IllegalArgumentException(String.format("转换后的key不能为null(原始key:%s)", key));
+        }
+        return convertedKey;
     }
 }
